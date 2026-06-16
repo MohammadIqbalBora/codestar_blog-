fc95a05760cf9671b578b0014cb39c96da6d8f05
IqbalBora <mi_bora@yahoo.co.uk>
2026-06-16 20:39:23 +0100

    deployment settings

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_9XMnbIHlNB6p@ep-twilight-queen-ascs1xxa.c-4.eu-central-1.aws.neon.tech/bulge_cone_upper_944795")
 
